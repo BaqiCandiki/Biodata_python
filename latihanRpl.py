95d314e8e5678b3f5f2f8ebb39974fcdb4401078
BaqiCandiki <140218550+BaqiCandiki@users.noreply.github.com>
2023-08-19 07:03:09 +0700

    Add files via upload

---
+++ latihanRpl.py
@@ -0,0 +1,3 @@
+print("Nama saya baqi")
+print("Kelas X RPL 1")
+print("Saya Mengambil RPL dikarnakan Saya menyukai Gim dan saya ingin menciptakan gim yang bisa menjadi terkenal suatu saat dan mendapatkan untung yyang besar, jadi saya mengambil RPL agar saya bisa membuat gim yang bagus")
